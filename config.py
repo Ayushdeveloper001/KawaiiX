245f99ec29b3dd3e9c0a5109779c8fe3fb7c7765
Ayush Pandey <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-12-10 10:59:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,79 @@
+import json
+import os
+
+
+def get_user_list(config, key):
+    with open(f"{os.getcwd()}/NekoRobot/{config}", "r") as json_file:
+        return json.load(json_file)[key]
+
+
+# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
+class Config(object):
+    LOGGER = True
+    # REQUIRED
+    # Login to https://my.telegram.org and fill in these slots with the details given by it
+
+    API_ID = 28397760  # integer value, dont use ""
+    API_HASH = "c5850a7b1efe26f5835a1c219e65ed54"
+    TOKEN = "6462054328:AAE0wg7qHMAzvyXb7NhSwrNI7WIM7EhQw0o"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    STRING_SESSION = ""
+    OWNER_ID = 644598176  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "HeenCollen"
+    SUPPORT_CHAT = "KawaiiAnimeChat"  # Your own group for support, do not add the @
+    JOIN_LOGGER = (
+        -1002058516246
+    )  # Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = (
+        -1002058516246
+    )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+
+    # RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = ""  # needed for any database modules
+    DB_URL = ""
+    REDIS_URL = "redis-13431.c305.ap-south-1-1.ec2.cloud.redislabs.com:13431"
+    LOAD = []
+    NO_LOAD = ["rss", "cleaner", "connection", "math"]
+    WEBHOOK = False
+    INFOPIC = True
+    URL = None
+    SPAMWATCH_API = "P5_FWlwgUrpchwJceZDUSDxa41G396dn7J0vSEMWeBhHJ6C4q8VJLzjhfZPxNKUZ"  # go to support.spamwat.ch to get key
+    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
+
+    # OPTIONAL
+    ##List of id's -  (not usernames) for users which have sudo access to the bot.
+    DRAGONS = get_user_list("elevated_users.json", "sudos")
+    ##List of id's - (not usernames) for developers who will have the same perms as the owner
+    DEV_USERS = get_user_list("elevated_users.json", "devs")
+    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    DEMONS = get_user_list("elevated_users.json", "supports")
+    # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    TIGERS = get_user_list("elevated_users.json", "tigers")
+    WOLVES = get_user_list("elevated_users.json", "whitelists")
+    DONATION_LINK = None  # EG, paypal
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
+    STRICT_GBAN = True
+    WORKERS = (
+        8  # Number of subthreads to use. Set as number of threads your processor uses
+    )
+    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
+    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
+    CASH_API_KEY = (
+        "VXRGGYORNRVWG9HG"  # Get your API key from https://www.alphavantage.co/support/#api-key
+    )
+    TIME_API_KEY = "QO71WZHGIT4V"  # Get your API key from https://timezonedb.com/api
+    WALL_API = (
+        "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
+    )
+    AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
+    BL_CHATS = []  # List of groups that you want blacklisted.
+    SPAMMERS = None
+
+
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True  # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
